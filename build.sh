b784ffd97ef58662612046142e45283260ea8e72
patarchims <patarchims@gmail.com>
2025-07-30 16:30:14 +0700

    read

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "pulling latest update from git"
+git pull
+
+echo "setup golang ..."
+export PATH=$PATH:/usr/local/go/bin
+
+ceho "Clear systectl log ... "
+journalctl --vacuum-time=1d
+
+echo "building..."
+go build -o hrd-api
+
+echo "restarting the service"
+systemctl restart rme.service
+systemctl restart rme.service
+
+echo "service status"
+systemctl status rme.service
+
+echo "done."
